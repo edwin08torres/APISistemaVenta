b529abc3ec21b6f4c78237cf6230c73a7cf4ab87
Edwin Torrez <80346774+edwin08torres@users.noreply.github.com>
2023-12-07 21:36:50 -0600

    upd api controllers
    Signed-off-by: Edwin Torrez <80346774+edwin08torres@users.noreply.github.com>

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVenta.API.Utilidad;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteService _clienteServicio;
+
+        public ClienteController(IClienteService clienteServicio)
+        {
+            _clienteServicio = clienteServicio;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var rsp = new Response<List<ClienteDTO>>();
+
+            try
+            {
+                rsp.status = true;
+                rsp.Value = await _clienteServicio.lista();
+
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+        }
+
+        [HttpPost]
+        [Route("Crear")]
+        public async Task<IActionResult> Crear([FromBody] ClienteDTO cliente)
+        {
+            var rsp = new Response<ClienteDTO>();
+
+            try
+            {
+                rsp.status = true;
+                rsp.Value = await _clienteServicio.Crear(cliente);
+
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody] ClienteDTO cliente)
+        {
+            var rsp = new Response<bool>();
+
+            try
+            {
+                rsp.status = true;
+                rsp.Value = await _clienteServicio.Editar(cliente);
+
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+
+        }
+
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var rsp = new Response<bool>();
+
+            try
+            {
+                rsp.status = true;
+                rsp.Value = await _clienteServicio.Eliminar(id);
+
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+
+            }
+            return Ok(rsp);
+
+        }
+    }
+}
